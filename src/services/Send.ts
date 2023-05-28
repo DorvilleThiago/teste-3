0a1abe37219ea1aae51eb2e2c7bacf5d3cb133ea
DorvilleThiago <thiago.dorville@souunit.com>
2023-05-28 14:22:30 -0300

    fix: android http

--- src/services/Send.ts
+++ src/services/Send.ts
@@ -1,6 +1,6 @@
 export async function Send(nome: string, numero: number, detalhes: string) {
     try {
-        const response = await fetch('http://localhost:9000/create', {
+        const response = await fetch('http://143.198.101.34:9000/create', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
